8297a3ebbd93217fe077502cf8de49f9223d1f63
Tatyana Burdienko <Tatyana.mishkileeva@mail.ru>
2024-02-02 18:34:59 +0900

    add HW:test1.1

---
+++ LPP/Homwork/test1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Write(FirstNumber: );
+int FirstNumber = int.Parse(Console.ReadLine());
+
+Console.Write(SecondNumber: );
+int SecondNumbe = int.Parse(Console.ReadLine());
+
+
+if (FirstNumber>SecondNumbe)
+{
+     Console.WriteLine(первое число больше);
+}
+if (FirstNumber<SecondNumbe)  
+{
+Console.WriteLine(второе число больше);
+} 
+if (FirstNumber=SecondNumbe)
+{
+     Console.WriteLine(числа равны);
+}
+    
+
