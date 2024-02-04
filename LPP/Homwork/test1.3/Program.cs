844f91a1015b2d1c05de0eff944a65b3b5166b70
Tatyana Burdienko <Tatyana.mishkileeva@mail.ru>
2024-02-04 14:05:52 +0900

    add HW:test1.3

---
+++ LPP/Homwork/test1.3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("number: ' ");
+int n = int.Parse(Console.ReadLine());
+if (n%2 ==0)
+{
+    Console.WriteLine("нечетное");
+}
+else 
+{
+    Console.WriteLine("четное");
+}
+
